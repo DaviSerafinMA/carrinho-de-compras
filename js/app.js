0581a2e937ccab0ab04753b59576ac955ec991da
Davi <macchioni.alfonso@gmail.com>
2025-11-12 15:51:00 -0300

    primeiro commit

---
+++ js/app.js
@@ -0,0 +1,15 @@
+// Passo a passo do projeto
+// 1 selecionar uma quantidade de produtos
+// 2 selecionar um produto
+// 3 fazer o produto selecionado ser adicionado ao carrinho
+// 4 somar totais do carrinho
+
+let produto = document.getElementById('produto')
+console.log(produto.innerText)
+
+function adicionar(){
+
+}
+function limpar(){
+    
+}
