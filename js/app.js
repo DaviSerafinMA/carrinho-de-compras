ae26ac67ac23429b76216bbde6f4280df4c685bb
Davi <macchioni.alfonso@gmail.com>
2025-11-17 14:03:30 -0300

    change the number of products without affect the shopping cart

--- js/app.js
+++ js/app.js
@@ -3,19 +3,20 @@
 // 2 selecionar um produto
 // 3 fazer o produto selecionado ser adicionado ao carrinho
 // 4 somar totais do carrinho
+
 let carrinho = document.getElementById('lista-produtos')
-let inputQnt = document.getElementById('quantidade')
+let inputQnt = document.getElementById('quantidade').value
 let htmlTotal = document.getElementById('valor-total')
 let valorTotal = parseInt(document.getElementById('valor-total').value)
 valorTotal = 0
-const carrinhoArray = []
-console.log(carrinhoArray)
-let id = parseInt(1)
+let carrinhoArray = []
+
 
 limpar();
 
 
 function adicionar(){//quando acionado a função
+    if(inputQnt>=1){
     let produtoElemento = document.getElementById('produto'); //define produto como a seleção atual da caixa
     let qnt = parseInt(document.getElementById('quantidade').value); //guarda como numero a quantidade escrita
     let nomeCompleto = produtoElemento.value; //transforma o elemento html em texto
@@ -24,22 +25,38 @@ function adicionar(){//quando acionado a função
     nome = nomeArray[0]; //faz o nome deixar de ser um array
     valorUnit = parseInt(nomeValor[1]); //faz o valor deixar de ser um array
     
-    subTotal = parseInt(valorUnit * qnt)
-    valorTotal = valorTotal + subTotal
-    htmlTotal.innerHTML = `R$ ${valorTotal}`
+    subTotal = parseInt(valorUnit * qnt); //calcula o total que sera adicionado
+    valorTotal = valorTotal + subTotal; //soma o valor anterior do carrinho com a nova adição
+    htmlTotal.innerHTML = `R$ ${valorTotal}` //altera o valor total exibido na pagina
 
+    //console.log(nome)
+    let id = nome
+    let returnId = document.getElementById(id)
     
-    carrinhoArray.push(nome)
-    console.log(carrinhoArray)
-    
-    carrinho.innerHTML = carrinho.innerHTML+`<section class="carrinho__produtos__produto"> 
-                            <span class="texto-azul">${qnt}x</span> ${nome} <span class="texto-azul">R$${valorUnit}</span>
-                        </section>`
-    if(inputQnt==''){
-        alert("Preencha o campo de quantidade!")
+    if(carrinhoArray.some(Element => Element == id)){
+        let selected = returnId.innerText
+        let add = selected.split("x")
+        let addQnt = parseInt(add[0])
+        qnt = qnt + addQnt
+        console.log(qnt)
+        returnId.remove()
+        carrinho.innerHTML = carrinho.innerHTML+`<section class="carrinho__produtos__produto" id="${id}"> 
+                                <span class="texto-azul">${qnt}x</span> ${nome} <span class="texto-azul">R$${valorUnit}</span>
+                            </section>`
+
+        console.log(carrinho.innerHTML)
+        console.log(returnId)
+    }else{
+        carrinhoArray.push(id);
+        console.log(carrinhoArray);
+        
+        carrinho.innerHTML = carrinho.innerHTML+`<section class="carrinho__produtos__produto" id="${id}"> 
+                                <span class="texto-azul">${qnt}x</span> ${nome} <span class="texto-azul">R$${valorUnit}</span>
+                            </section>`
+        };
+    }else{
+        alert("Insira uma quantidade válida")
     }
-    id = id + parseInt(1)
-    console.log(id)
 };
 
 function limpar(){
